68db439b5d9f96bf73a564899974ce9952a10ff8
Bobby Grayson <bobbygrayson@gmail.com>
2015-02-26 19:40:53 -0500

    fix weird fuckup

--- baller_app/app/main/controllers/main_controller.rb
+++ baller_app/app/main/controllers/main_controller.rb
@@ -2,15 +2,20 @@
 class MainController < Volt::ModelController
   model :store
   def index
+    # Add code for when the index view is loaded
   end
 
   def about
+    # Add code for when the about view is loaded
   end
 
-  def add_link
-    _links << { description: page._new_description,
-                url: page._new_url }
-    go '/links'
+  def add_todo
+    _todos << { name: page._new_todo }
+    page._new_todo = ''
+  end
+
+  def remove_todo(todo)
+    _todos.delete(todo)
   end
 
   private
