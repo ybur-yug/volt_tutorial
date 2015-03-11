317b3597bcca6b3c6c535b2b4401a8e13fdcd4d1
Robert H Grayson II <bobbygrayson@gmail.com>
2015-03-11 11:51:51 -0400

    add percentage complete count

--- appname/app/main/controllers/main_controller.rb
+++ appname/app/main/controllers/main_controller.rb
@@ -19,9 +19,12 @@ class MainController < Volt::ModelController
   def completed
     _todos.count { |v| v._complete.true? }
   end
-
+  
+  def percent_complete
+    (completed / _todos.count.to_f * 100).round 
+  end
   private
-
+  
   # The main template contains a #template binding that shows another
   # template.  This is the path to that template.  It may change based
   # on the params._controller and params._action values.
